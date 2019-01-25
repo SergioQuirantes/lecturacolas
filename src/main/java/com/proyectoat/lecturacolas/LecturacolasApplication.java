aef3ff099b8930e09a366ba6b11a82d1ce0299fd
Sergio Quirantes Vallés <squirantes@atsistemas.com>
2019-01-25 10:34:44 +0100

    primer commit

---
+++ src/main/java/com/proyectoat/lecturacolas/LecturacolasApplication.java
@@ -0,0 +1,14 @@
+package com.proyectoat.lecturacolas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LecturacolasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LecturacolasApplication.class, args);
+	}
+
+}
+
